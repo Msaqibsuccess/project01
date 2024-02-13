869d27ccd119da34ba4034dab0f0098cce0f246b
Muhammadsaqibgood <159001445+Muhammadsaqibgood@users.noreply.github.com>
2024-02-13 10:07:44 -0800

    Add files via upload

---
+++ qrcode.js
@@ -0,0 +1,20 @@
+const qrinput = document.getElementById('qr-input');
+const qrbutton = document.getElementById('qr-button');
+const qrimg = document.getElementById('qr-img');
+
+console.log(qrinput, qrbutton, qrimg);
+
+
+qrbutton.addEventListener('click', function () {
+
+    const inputValue = qrinput.value;
+    console.log(inputValue);
+
+    if (!inputValue) {
+        alert("Please enter a valid URL");
+        return;
+    } else {
+        qrimg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inputValue}`;
+        arimg.alt = `QR Code for ${inputValue}`;
+    }
+})
